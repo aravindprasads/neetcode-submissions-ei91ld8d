fd02c846db154205d1b6ae9221806ba3889b18a9
Aravind Prasad S <raja.avi@gmail.com>
2026-06-03 16:39:02 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> hash;
+        for(auto it:strs) {
+            string st_copy = it;
+            sort(it.begin(), it.end());
+            hash[it].push_back(st_copy);
+        }
+
+        vector<vector<string>> ans;
+        for(auto it:hash) {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
