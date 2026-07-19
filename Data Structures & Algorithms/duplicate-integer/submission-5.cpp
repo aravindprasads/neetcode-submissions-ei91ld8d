5250b07f8554edb2b3f9ecddfbf081a78a5d3989
Aravind Prasad S <raja.avi@gmail.com>
2026-07-19 05:46:40 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int, bool> hash;
+        for(auto it:nums) {
+            if(hash.find(it) != hash.end())
+                return true;
+            hash[it] = true;
+        }
+        return false;
+    }
+};
