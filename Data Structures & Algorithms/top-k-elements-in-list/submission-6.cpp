3a17d723eeb96d31f2c6ac8bacce91be02eeb243
Aravind Prasad S <raja.avi@gmail.com>
2026-07-20 09:43:58 +0530

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        unordered_map<int, int> hash;
+        for(auto it:nums) {
+            hash[it]++;
+        }
+
+        priority_queue<tuple<int, int>> heap;
+        for(auto it:hash) {
+            heap.push({it.second, it.first});
+        }
+
+        vector<int> ans;
+        while(k && !heap.empty()) {
+            auto [freq, val] = heap.top();
+            ans.push_back(val);
+            heap.pop();
+            k--;
+        }
+        return ans;
+    }
+};
