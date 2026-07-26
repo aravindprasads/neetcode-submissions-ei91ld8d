e3f00872904feab2f64b36527cc92b9e80c9aa9d
Aravind Prasad S <raja.avi@gmail.com>
2026-07-26 10:49:29 +0530

    Add: count-number-of-islands - submission-3

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.cpp
@@ -0,0 +1,59 @@
+/*0,0
+
+0,1
+0,-1
+1,0
+-1,0
+*/
+
+class Solution {
+public:
+
+    void bfs(vector<vector<char>>grid, unordered_set<string>& visited, int row, int col) {
+        string key = to_string(row) + "," + to_string(col);
+        if(visited.find(key) != visited.end())
+            return;
+
+        visited.insert(key);
+
+        vector<int> dr = {0,0,1,-1};
+        vector<int> dc = {1,-1,0,0};
+
+        for(int k=0; k<4; k++) {
+            int nr = dr[k] + row;
+            int nc = dc[k] + col;
+
+            if((nr < 0) || (nr >= grid.size()))
+                continue;
+            if((nc < 0) || (nc >= grid[row].size()))
+                continue;
+
+            if(grid[nr][nc] == '0')
+                continue;
+
+            bfs(grid, visited, nr, nc);
+        }
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.size() == 0)
+            return 0;
+
+        unordered_set<string> visited;        
+        int cnt = 0;
+
+        for(int r=0; r<grid.size(); r++) {
+            for(int c=0; c<grid[r].size(); c++) {                
+                if(grid[r][c] == '0')
+                    continue;
+                string key = to_string(r) + "," +  to_string(c);
+                if (visited.find(key) != visited.end())
+                    continue;
+                bfs(grid, visited, r, c);
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
