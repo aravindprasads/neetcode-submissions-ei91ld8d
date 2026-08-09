6c26f7fb604518c28305ea5f04242e8cda40e0f6
Aravind Prasad S <raja.avi@gmail.com>
2026-08-09 11:42:46 +0530

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    bool helper(string s, int index, unordered_set<string> dic, 
+                unordered_map<int, bool>& dp) {
+        if(index >= s.length())
+            return true;
+        
+        if(dp.find(index) != dp.end())
+            return dp[index];        
+
+        string new_s = "";
+        for(int i=index; i<s.length(); i++) {
+            new_s += s[i];
+            // cout<<new_s<<endl;
+            if(dic.find(new_s) != dic.end()) {
+                // cout<<"call "<<new_s<<endl;
+                if(helper(s, i+1, dic, dp)) {
+                    dp[index] = true;
+                    return true;
+                }
+            }
+        }
+        dp[index] = false;
+        return false;
+    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+
+        unordered_map<int, bool> dp;
+        unordered_set<string> dic;
+        for(auto it:wordDict)
+            dic.insert(it);
+
+        return(helper(s, 0, dic, dp));
+    }
+};
